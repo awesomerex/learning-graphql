aceb1cd49b70bada06cc3c09a8ff4bb94d675f09
Rex Soriano <rex@rex-soriano.com>
2019-01-08 21:55:40 -0500

    Lecture 20

--- src/index.js
+++ src/index.js
@@ -1,11 +1,68 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+//Demo user data
+const users = [{
+    id: '1',
+    name: 'Rex',
+    email: 'rex@example.com',
+    age: 30
+},{
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com'
+}, {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com'
+}]
+
+const posts = [{
+    id: '1',
+    title: 'Title',
+    body: 'the body',
+    published: true,
+    author: 1
+}, {
+    id: '2',
+    title: 'Title 2',
+    body: 'the body 2',
+    published: true,
+    author: 2
+}, {
+    id: '3',
+    title: 'Title 3',
+    body: 'the body 3',
+    published: false,
+    author: 3
+}]
+
+const comments = [{
+    id: '1',
+    text: 'Stuff'
+}, {
+    id: '2',
+    text: 'Great!'
+}, {
+    id: '3',
+    text: 'Cool stuff!'
+}, {
+    id: '4',
+    text: 'Great post!'
+}]
+
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        greeting(name: String): String!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        comments(query: String): [Comment!]!
         me: User!
         post: Post!
+    }
+    
+    type Comment {
+        id: ID!
+        text: String!
     }
     
     type User {
@@ -13,6 +70,7 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
     }
     
     type Post {
@@ -20,6 +78,7 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
     }
 `
 
@@ -29,6 +88,16 @@ const typeDefs = `
 // parent, args, context, info
 const resolvers = {
     Query: {
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
+            }
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+
+        },
         me() {
             return {
                 id: '123098',
@@ -45,15 +114,37 @@ const resolvers = {
                 published: true
             }
         },
-        greeting(parent, args, ctx, info) {
-            if(args.name){
-                return `Hello, ${args.name}!`
+        posts(parent, args, ctx, info){
+            if(!args.query) {
+                return posts
             }
-            else{
-                return 'Hello!'
+
+            return posts.filter((post) => {
+                if( post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+                post.body.toLowerCase().includes(args.query.toLowerCase())){
+                    return post
+                }
+            })
+        },
+        comments(parent, args, ctx, info){
+            if (!args.query){
+                return comments
             }
         }
-
+    },
+    Post: {
+        author(parent, args, ctx, info){
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info){
+            return posts.find((post) => {
+                return post.author === parent.id
+            })
+        }
     }
 }
 
